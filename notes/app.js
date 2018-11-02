3c5425902886bea433dc844cff8957ca23f1f359
Pako Herrera <pakman_198@hotmail.com>
2018-11-02 15:03:35 -0600

    Node.js Fundamentals (Notes App)

---
+++ notes/app.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js')
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+const bodyOptions = {
+    describe: 'Content of note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Delete a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
+var command = argv._[0];
+
+if(command === 'add') {
+    const note = notes.addNote(argv.title, argv.body);
+    if(note) {
+        console.log('Note created');
+       notes.logNote(note);
+    } else {
+        console.log('Note title taken.')
+    }
+
+} else if(command === 'list') {
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s)`);
+    allNotes.forEach(note => notes.logNote(note));
+
+} else if(command === 'read') {
+    const note = notes.getNote(argv.title);
+
+    if(note) {
+        console.log('Note found');
+        notes.logNote(note);
+    } else {
+        console.log('Note not found');
+    }
+
+} else if(command === 'remove') {
+    const removedNote = notes.removeNote(argv.title);
+    var message = removedNote ? 'Note was removed' : 'Note not found';
+    console.log(message);
+
+} else {
+    console.log('Command not recognized');
+}
---
+++ notes/playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+    name: 'Pako'
+}
+
+person.age = 30;
+
+debugger;
+
+person.name = 'Andrew';
+
+console.log(person);
